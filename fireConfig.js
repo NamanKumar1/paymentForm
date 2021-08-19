f4fa7453f38a6636c27d54d63df6ce9c6fd81f7e
NamanKumar1 <naman.kumar_cs19@gla.ac.in>
2021-08-19 17:04:05 +0530

    add firebase configuration

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDC8cXYH_Y90SQfFQ0QJD3wuVVXeQmZvYE",
+    authDomain: "payment-form-dd59e.firebaseapp.com",
+    databaseURL: "https://payment-form-dd59e-default-rtdb.firebaseio.com",
+    projectId: "payment-form-dd59e",
+    storageBucket: "payment-form-dd59e.appspot.com",
+    messagingSenderId: "584329821047",
+    appId: "1:584329821047:web:dec28c5aab93c0c745ff72"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
